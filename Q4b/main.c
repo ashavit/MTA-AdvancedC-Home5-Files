7d07e008dd6a3d2a329344af3d2e7aaceea03c5b
Amir Shavit <amir.shavit@autodesk.com>
2017-01-25 00:12:10 +0200

    Q4b: Import given main

--- Q4b/main.c
+++ Q4b/main.c
@@ -3,16 +3,34 @@
 #include "Constants.h"
 #include "Students.h"
 
+void printNames(char **records,int size);
+void freeRecords(char **records,int size);
+
+/* Main provided for excercise */
 int main(int argc, char **argv)
 {
-    // Make sure we have all argc
-    if (argc != 2)
-        exit(MAIN_ARGS_ERROR);
+    char **records;
+	int resSize;
 
-    char *dbFileName = argv[1];
-    // printf("%s\n", dbFileName);
+	records = findAverageGrade(argv[1], 85, &resSize);
+	printNames(records,resSize);
 
-    sortStudentIndexesForFile(dbFileName);
+	freeRecords(records,resSize);
 
     return 0;
+}
+
+void printNames(char **records,int size)
+{
+	int i;
+	printf("The students names are:\n");
+	for (i=0 ; i<size ;i++ )
+		printf("%s\n" , records[i]);
+}
+
+void freeRecords(char **records,int size)
+{
+	for (int i = 0; i < size; i++)
+		free(records[i]);
+    free(records);
 }
